f03720cc61b27aa481dc40e7f9afb1bc77425340
Saul Johnson <saul.a.johnson@gmail.com>
2016-07-14 11:54:24 +0100

    Document IDocument

--- Sulfide/IDocument.cs
+++ Sulfide/IDocument.cs
@@ -7,18 +7,41 @@ namespace Sulfide
     /// </summary>
     public interface IDocument
     {
+        /// <summary>
+        /// Gets the save strategy for the document.
+        /// </summary>
         ISaveStrategy SaveStrategy { get; }
 
+        /// <summary>
+        /// Gets the clipboard strategy for the document.
+        /// </summary>
         IClipboardStrategy ClipboardStrategy { get; }
 
+        /// <summary>
+        /// Gets the printing strategy for the document.
+        /// </summary>
         IPrintingStrategy PrintingStrategy { get; }
 
+        /// <summary>
+        /// Gets the history strategy for the document.
+        /// </summary>
         IHistoryStrategy HistoryStrategy { get; }
 
+        /// <summary>
+        /// Gets or sets the window title for the document.
+        /// </summary>
         string Text { get; set; }
 
+        /// <summary>
+        /// Shows the document in the editor.
+        /// </summary>
+        /// <param name="dockPanel">The dock panel to show the document in.</param>
+        /// <param name="dockState">The initial dock state of the document.</param>
         void Show(DockPanel dockPanel, DockState dockState);
 
+        /// <summary>
+        /// Closes the document in the editor.
+        /// </summary>
         void Close();
     }
 }
